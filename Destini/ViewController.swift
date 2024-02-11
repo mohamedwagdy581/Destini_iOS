eba0722b309631f7ff1264a357e0e5fbebf688e7
mohamedwagdy581 <mw41532@gmail.com>
2024-02-11 14:30:58 +0300

    Initial Commit

---
+++ Destini/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Destini
+//
+//  Created by Mohamed Wagdy on 11/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
