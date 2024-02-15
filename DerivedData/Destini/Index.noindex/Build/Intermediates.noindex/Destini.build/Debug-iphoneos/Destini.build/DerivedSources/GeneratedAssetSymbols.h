915c0acbcc74c3a452865b274014682f9fc7cfc8
mohamedwagdy581 <mw41532@gmail.com>
2024-02-15 14:22:23 +0300

    Destini App iOS

---
+++ DerivedData/Destini/Index.noindex/Build/Intermediates.noindex/Destini.build/Debug-iphoneos/Destini.build/DerivedSources/GeneratedAssetSymbols.h
@@ -0,0 +1,18 @@
+#import <Foundation/Foundation.h>
+
+#if __has_attribute(swift_private)
+#define AC_SWIFT_PRIVATE __attribute__((swift_private))
+#else
+#define AC_SWIFT_PRIVATE
+#endif
+
+/// The "LABLOGONewRound2" asset catalog image resource.
+static NSString * const ACImageNameLABLOGONewRound2 AC_SWIFT_PRIVATE = @"LABLOGONewRound2";
+
+/// The "background" asset catalog image resource.
+static NSString * const ACImageNameBackground AC_SWIFT_PRIVATE = @"background";
+
+/// The "labBackground" asset catalog image resource.
+static NSString * const ACImageNameLabBackground AC_SWIFT_PRIVATE = @"labBackground";
+
+#undef AC_SWIFT_PRIVATE
